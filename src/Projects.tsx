5d05ca325a2c8375a509d0a021e6177986bce3c9
Tomas <tomaserobino@gmail.com>
2024-07-19 16:48:53 +0200

    correctly made projects sticky

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,10 +1,10 @@
-import styles from "./App.module.css";
+import styles from "./Projects.module.css";
 import React from "react";
 
 export default function Projects() {
   return (
     <div>
-      <h1 className={styles.title}>Projects</h1>
+
     </div>
   )
 }
