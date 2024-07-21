e869becc47d48ab34867607eb7d80d91e22aa7cd
Tomas <tomaserobino@gmail.com>
2024-07-21 04:22:07 +0200

    cardGroup offset is now dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,19 @@ import Project from "./Project.tsx";
 
 function App() {
   const viewportPos = (window.innerHeight/100)*40;
+  const projectGroupRef = useRef<HTMLDivElement>(null);
   const projectsRef = useRef<HTMLHeadingElement>(null);
-  const [divPos, setDivPos] = useState(-300);
+  const [divPos, setDivPos] = useState(-5000);
+
+  useEffect(() => {
+    if (!projectGroupRef.current) return;
+    //setProjectGroupWidth(projectGroupRef.current?.getBoundingClientRect().width);
+    const children = projectGroupRef.current.children;
+    const res = "-" + (( children[0].getBoundingClientRect().width + parseFloat(getComputedStyle(children[0]).marginRight) ) * children.length).toString() + "px";
+    projectGroupRef.current.style.right = res;
+    setDivPos(parseFloat(res))
+    //group.style.right = "-1000px";
+  }, [])
 
   useEffect(() => {
     window.addEventListener('wheel', handleScroll);
@@ -24,7 +35,7 @@ function App() {
       document.body.style.overflow = "hidden";
       if (event.deltaY < 0) {
         setDivPos(divPos-80);
-        if (divPos <= -300) document.body.style.overflow = "auto";
+        if (divPos <= -5000) document.body.style.overflow = "auto";
       } else setDivPos(divPos+80);
 
     }
@@ -36,10 +47,12 @@ function App() {
       <Header />
       <div style={{ backgroundColor: "black", height: "200vh" }}></div>
       <h1 className={styles.title} id="projectsHeader" ref={projectsRef}>Projects</h1>
-      <div style={{ position: "sticky", top: "25vh" }}>
-        <Project offset={0} right={divPos} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
-        <Project offset={(window.innerWidth/100)*40 + 800} right={divPos} image={"https://picsum.photos/200"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
-        <Project offset={(window.innerWidth/100)*40*2 + 800*2} right={divPos} image={"https://picsum.photos/200"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+      <div style={{ position: "sticky", top: "25vh"}}>
+        <div ref={projectGroupRef} className={styles.cardGroup} style={{ right: divPos }}>
+          <Project offset={0} right={divPos} image={"https://picsum.photos/1000"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+          <Project offset={(window.innerWidth/100)*40 + 800} right={divPos} image={"https://picsum.photos/200"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+          <Project offset={(window.innerWidth/100)*40*2 + 800*2} right={divPos} image={"https://picsum.photos/200"} title={"Example project"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum dictum nisi, eget vestibulum mi tempus sit amet. Nulla lobortis elementum mauris, et auctor sapien condimentum mattis."}/>
+        </div>
       </div>
       <div style={{ backgroundColor: "black", height: "200vh" }}></div>
     </div>
--- src/Project.tsx
+++ src/Project.tsx
@@ -3,20 +3,8 @@ import React from "react";
 import {Card, CardContent, CardHeader, CardMedia} from "@mui/material";
 
 export default function Project(props: { offset: number, right: number, title: string, image: string, description: string }) {
-  /*
   return (
-    <Card className={styles.card} style={{ right: props.right }}>
-      <CardContent>
-        <CardHeader title={props.title} className={styles.cardHeader}/>
-        <CardMedia component="img" alt={props.title} image={props.image} className={styles.cardMedia}/>
-        <p>{props.description}</p>
-      </CardContent>
-    </Card>
-  )
-   */
-
-  return (
-    <div style={{ right: props.right-props.offset }} className={styles.card}>
+    <div className={styles.proj}>
       <img src={props.image} alt={props.title} className={styles.img}/>
       <div className={styles.titleNDesc}>
         <h2>{props.title}</h2>
